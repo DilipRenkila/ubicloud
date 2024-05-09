55a85d1d196021479878f16aed13f33b02d56d61
Enes Cakir <enes@ubicloud.com>
2024-05-09 22:52:08 +0300

    Use provisioned at instead of display state to pick a vm from the pool
    The 'display_state' function is used to show the current state of the
    virtual machines to the customer, and it's dynamically calculated in
    certain cases. The 'display' prefix implies that we shouldn't use it
    into our backend logics. 'provisioned_at' serves as a more reliable
    indicator to determine whether the virtual machine has been provisioned.

--- spec/model/vm_pool_spec.rb
+++ spec/model/vm_pool_spec.rb
@@ -21,17 +21,17 @@ RSpec.describe VmPool do
     end
 
     it "returns nil if there are no vms in running state" do
-      vm.update(display_state: "creating")
+      vm.update(provisioned_at: nil)
       expect(pool.pick_vm("new-name")).to be_nil
     end
 
     it "returns nil if there are no untainted vms" do
-      vm.update(display_state: "running", tainted_at: Time.now)
+      vm.update(provisioned_at: Time.now, tainted_at: Time.now)
       expect(pool.pick_vm("new-name")).to be_nil
     end
 
     it "returns the vm if there is untainted one in running state" do
-      vm.update(display_state: "running", tainted_at: nil)
+      vm.update(provisioned_at: Time.now, tainted_at: nil)
       expect(pool.pick_vm("new-name").id).to eq(vm.id)
     end
   end
